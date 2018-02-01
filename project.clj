bf1265274bc7237b8957fdd34db5430a37041200
Dan Stone <danielstone@riverford.co.uk>
2018-02-01 09:28:46 +0000

    bumped to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject riverford/objection "0.1.0"
+(defproject riverford/objection "0.1.1-SNAPSHOT"
   :description "Manages global resources."
   :url "https://github.com/riverford/objection"
   :license {:name "Eclipse Public License"
